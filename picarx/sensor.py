b81096fe9a426ab53825c8329bb0a66f0e356e66
ta-wells <ta.wells1216@gmail.com>
2025-01-26 13:37:43 -0800

    Update sensor.py

--- picarx/sensor.py
+++ picarx/sensor.py
@@ -88,7 +88,7 @@ class Interpreter():
 
 class Control():
 
-    def __init__(self,kp=50,target = 0,sat=25):
+    def __init__(self,kp=20,target = 0,sat=25):
         self.kp = kp
         self.sat = sat
         self.target = target
