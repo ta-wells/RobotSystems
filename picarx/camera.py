8cb0514932361a712f80ba464bddf9469f6fdcbb
ta-wells <ta.wells1216@gmail.com>
2025-01-30 12:21:57 -0800

    Update camera.py

--- picarx/camera.py
+++ picarx/camera.py
@@ -55,7 +55,7 @@ class Camera():
         
         # Convert to grayscale
         img = cv2.imread(self.currentPhoto)
-        crop_img = img[0:120, 0:160]
+        crop_img = img[0:120, 0:80]
         #crop_img = img
         gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
         # Gaussian blur
