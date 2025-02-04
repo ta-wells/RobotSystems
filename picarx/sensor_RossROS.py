a2a2d8f49769eb9a9d0ad345f0a932938b9bd450
ta-wells <ta.wells1216@gmail.com>
2025-02-04 13:14:57 -0800

    Update sensor_RossROS.py

--- picarx/sensor_RossROS.py
+++ picarx/sensor_RossROS.py
@@ -168,7 +168,7 @@ class Control():
                 logging.info(angle_set)
                 Angle_Bus_Class.write(angle_set)
                 px.set_dir_servo_angle(angle_set)
-                px.forward(40)
+                px.forward(0)
                 time.sleep(delay)
             except:
                 logging.debug("Not initialized --- skipping")
@@ -214,9 +214,9 @@ if __name__=='__main__':
     int = Interpreter()
     con = Control()
 
-    bread = rr.Bus(sn.read(), "Grayscale reading Bus")
-    bprocess = rr.Bus(int.process(), "Data processing Bus")
-    bcontrol = rr.Bus(con.proportional_control(), "Control bus")
+    bread = rr.Bus(sn.read, "Grayscale reading Bus")
+    bprocess = rr.Bus(int.process, "Data processing Bus")
+    bcontrol = rr.Bus(con.proportional_control, "Control bus")
     bTerminate = rr.Bus(0, "Termination Bus")
 
 
