415a26c04a8544a9542e612118740393a80d98d4
ta-wells <ta.wells1216@gmail.com>
2025-02-27 13:40:30 -0800

    Update ColorTracking_Perception.py

--- ArmPi/Functions/ColorTracking_Perception.py
+++ ArmPi/Functions/ColorTracking_Perception.py
@@ -474,10 +474,10 @@ class Color_Perception():
                 self.draw_color = (0, 0, 0)
                 self.detect_color = "None"
         
-            logging.info(self.detect_color)  
-            logging.info(self.start_pick_up)  
-            cv2.putText(self.img_copy, "Color: " + self.detect_color, (10, self.img_copy.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, self.draw_color, 2)
-            return self.detect_color,self.start_pick_up #Have to return this for now?
+        logging.info(self.detect_color)  
+        logging.info(self.start_pick_up)  
+        cv2.putText(self.img_copy, "Color: " + self.detect_color, (10, self.img_copy.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, self.draw_color, 2)
+        return self.detect_color,self.start_pick_up #Have to return this for now?
 
 if __name__ == '__main__':
     init()
